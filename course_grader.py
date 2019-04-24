23cbf132a70169098995d7073ed4e793727625fc
bronsonnaylor <49175664+bronsonnaylor@users.noreply.github.com>
2019-04-23 19:21:37 -0600

    Update course_grader.py

--- course_grader.py
+++ course_grader.py
@@ -1,2 +1,25 @@
-def course_grader(param):
-    return ""
+def course_grader(test_scores):
+    # Your code here
+    cumulative = 0
+    divide_by = 0
+    score = 0
+    for each in test_scores:
+        if each < 50:
+            return("fail")
+        cumulative = cumulative + each
+        divide_by += 1
+    score = cumulative / divide_by
+    if score >= 70:
+        return("pass")
+    return("fail")
+
+def main():
+    print(course_grader([100,75,45]))     # "fail"
+    print(course_grader([100,70,85]))     # "pass"
+    print(course_grader([80,60,60]))      # "fail"
+    print(course_grader([80,80,90,30,80]))  # "fail"
+    print(course_grader([70,70,70,70,70]))  # "pass"
+    print(course_grader([60,80,80,70,70]))  # "pass"
+
+if __name__ == "__main__":
+main()
